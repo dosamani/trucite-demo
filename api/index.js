0bb34d506db59d1be056cd99054b39b5d8f9bf7c
dosamani <rleggs@gmail.com>
2025-09-25 11:33:06 -0500

    Delete api/index.js

--- api/index.js
+++
@@ -1,9 +0,0 @@
-// api/index.js
-export default async function handler(req, res) {
-  res.status(200).json({
-    ok: true,
-    service: "TruCite MVP",
-    message: "Backend is alive",
-    time: new Date().toISOString(),
-  });
-}
